429737571c4d6c5e1152635f026af97ffc490c9d
wonjooncha <chawonjun11@gmail.com>
2014-01-18 21:16:33 +0900

    first log

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wjcha::Application.config.secret_key_base = '4d96fdb5b9871769112cef0d61241901011b477baa2fcbfd8ecb3fd1d69d92311aaba2f29745f42d52ef40b3cd5a7c202b6599618f5dd78de7e2c4df57042f03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wjcha::Application.config.session_store :cookie_store, key: '_wjcha_session'
